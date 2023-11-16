8e0cb863ba7fec01e3dddae97d0a951d2fe468d5
student <student@vlab.local>
2023-11-16 09:52:43 +0100

    Initial commit

---
+++ src/Abstract.java
@@ -0,0 +1,15 @@
+public class Abstract {
+    abstract class Animal{
+        private String name;
+        private int age;
+        private int weight;
+
+        public Animal(){
+            age = weight = 0;
+            name = "NN";
+        }
+    }
+
+
+
+}
---
+++ src/BirdClass.java
@@ -0,0 +1,2 @@
+public class BirdClass {
+}
